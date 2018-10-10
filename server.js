2466b7639fcd8302a6667f23cfcc7b39a8b232a7
fariswidhi <fariswidhiarta123@gmail.com>
2018-10-10 10:00:26 +0700

    push

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
-const io = require('socket.io')();
+const express = require('express');
+const http = require('http');
+const socketIo  = require('socket.io');
 
+const port = process.env.PORT || 4001;
+const index = require("./routes/index");
 
+const app = express();
+app.use(index);
 
-io.on('connection',(client)=>{
-	client.on('subscribeToTimer',(interval)=>{
-		console.log("client is subscribing with interval ",interval);
-		setInterval(()=>{
-			client.emit('timer',new Date());
-		},interval);
-	});
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const axios = require('axios');
+
+io.on('connection',socket=>{
+	console.log("Client Connected",setInterval(()=>getApiAndEmit(socket),1000));
+	socket.on('disconnect',()=>console.log("Client disconnect"));
 });
 
+const getApiAndEmit = async socket => {
+	try{
+		const res = await axios.get("https://api.darksky.net/forecast/7d530527ba3628f4e48509559ddb8203/43.7695,11.2558");
+		socket.emit("FromAP",res.data.currently.temperature);
 
-const port = 8000;
-io.listen(port);
-console.log("Listen on port",port);
+	}
+	catch(err){
+		console.log("error",err);
+	}
+}
 
-
+server.listen(port,()=> console.log("Listen on port",port));
--- src/App.js
+++ src/App.js
@@ -1,29 +1,45 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { subscribeToTimer } from './api';
+import socketIOClient from 'socket.io-client';
 
 class App extends Component {
 
-  constructor(props){
+  constructor(){
 
 
-    super(props);
+    super();
 
-    subscribeToTimer((err,timestamp) => this.setState({
-      timestamp
-    }));
     this.state = {
-      timestamp: 'no timestamp' 
-    }    ;
+      response: false,
+      endpoint: "http://127.0.0.1:4001/"
+    };
+
+  }
+
+  componentDidMount(){
+
+    const { endpoint } = this.state;
+    const socket = socketIOClient(endpoint);
+    socket.on("FromAP",data=>this.setState({response:data}));
+    console.log(this.state.endpoint);
+    console.log(this.state.response);
+
+
 
   }
   render() {
+    const {response} = this.state;
+
     return (
-      <div className="App">
-        <p className="App-intro">
-        this is the timer value: {this.state.timestamp}
-        </p>
+
+      <div style={{ textAlign: "center" }}>
+        {response
+          ? <p>
+              The temperature in Florence is: {response} °F
+            </p>
+          : <p>Loading...</p>
+        }
       </div>
     );
   }
